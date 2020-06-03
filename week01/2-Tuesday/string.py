16114b097c22e6797844d1047f46dce25d2069b2
Cainan Barboza <CainanBarboza1@gmail.com>
2020-06-03 09:06:19 -0500

    changes

---
+++ week01/2-Tuesday/string.py
@@ -0,0 +1,150 @@
+print("hello world")
+print('hello I\'m world')
+
+print('I am one line.\nI am another line')
+print('I am one line.\bI went back one space')
+print('I am one line.\tI horizontal spacing')
+print('I am one line.\nI new line')
+print('I am one line.\n\vI new line with vertical spacing')
+
+# print("""
+# We the People of the United States, 
+# \n\vin Order to form a more perfect Union, 
+# establish Justice, insure domestic Tranquility, 
+# provide for the common defence, 
+# promote the general Welfare, 
+# and secure the Blessings of Liberty to ourselves and our Posterity, 
+# do ordain and establish this Constitution for the United States of America.
+# """)
+
+print(2 ** 4)
+
+name = "Cainan"
+
+print(name)
+
+
+# f_name = "Michael"
+# l_name = "Cortez"
+
+
+
+
+# greeting = "Hello {} {}".format("Joe", "Varghese")
+# greeting = "Hello {} {}".format(f_name, l_name)
+
+# print(greeting)
+
+# f_name = "Woody"
+# l_name = "Connell"
+
+# greeting = "Hello {} {}".format(f_name, l_name)
+# print(greeting)
+# name = "Woody"
+# lname = "Connell"
+# name2 = "Michael"
+# f"Hello {name}"
+# greeting = f"Hello {name} {lname} and {name2}"
+
+# print(greeting)
+
+# num = 34
+# type(5)
+# print(type(num))
+
+# result = isinstance("5", str)
+# print(result)
+
+# myNum = "4"
+
+# num = 4
+# result = myNum + num
+# print(result)
+
+# print("one", "two", "three")
+# print(abs(-10))
+
+# num = 2
+# base = 10
+# exp = 3
+# print(base ** exp)
+# print(pow(base,exp))
+
+# cli = input("What is your name >>> ")
+
+# print(f"Hello {cli}, how are you doing?")
+
+# num = input("Please enter a number >> ")
+
+# result = float(num) * 0.0825
+# print(result)
+# a = "4"
+# print(type(a))
+# a_int = int(a)
+# print(type(a_int))
+
+# spam = "7"
+# spam = spam + "0"
+# eggs = int(spam) + 3
+# print(float(eggs))
+
+# 73.0
+# 82
+
+age = 15
+
+# if(age == 25):
+#     print("age is equal to 25")
+
+# if(age <= 25):
+#     print(f"age is <= 25")
+
+# if(age != 25):
+#     print(f"age is != 25")
+
+# if(age < 25):
+#     print(f"age < 25")
+
+# age = int(input("How old are you? >> "))
+# if(age >= 21):
+#     print('You can drink beer')
+# elif (age < 18):
+#     print("what are you even doing here?")
+# else:
+
+name = "woody"
+print(len(name))
+   
+
+
+
+
+
+
+# name_of_user = input("What is your name? ")
+# length_of_name = len(name_of_user)
+
+# if length_of_name > 10:
+#     print("That's too much name!!!!!")
+# elif length_of_name > 1:
+#     name_of_friend = input("What is your friend's name? ")
+#     greeting = f"Hello {name_of_user}, it is very nice to meet you and your friend {name_of_friend}!"
+#     print(greeting)
+# elif length_of_name > 0:
+#     print("That must be your initials")    
+# else:
+#     print("OK, I'll ask again some other time.")
+
+# count = 0
+
+# while count < 10:
+#     count += 1
+#     print(f"The count is: {count}")
+
+answer = ""
+
+while answer != "when" :
+    answer = input("Say when: ")
+    answer = answer.lower()
+
+print("Finished")   
